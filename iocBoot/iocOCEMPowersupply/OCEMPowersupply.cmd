9010ab97ce4dd786bccce6dcbba79d6a02d556e0
alessandro D'Uffizi <alessandro.duffizi@lnf.infn.it>
2025-07-22 10:07:43 +0000

    added unimag mask

--- iocBoot/iocOCEMPowersupply/OCEMPowersupply.cmd
+++ iocBoot/iocOCEMPowersupply/OCEMPowersupply.cmd
@@ -18,11 +18,6 @@ drvModbusAsynConfigure("command_port", "MODBUS_IP", 1, 6, 0, 3, 0, 1000, "OCEM")
 
 drvModbusAsynConfigure("readback_all", "MODBUS_IP", 1, 4, 20, 7, 0, 1000, "OCEM")
 
-## define ASYN readback_port port to access Read Input Registers 
-#drvModbusAsynConfigure("readback_port", "MODBUS_IP", 1, 4, 32, 10, 0, 1000, "GenericDevice")
-
-## define ASYN MODBUS_AI2 port to access to read load values
-#drvModbusAsynConfigure("readback_load", "MODBUS_IP", 1, 4, 52, 9, 0, 1000, "OCEM")
 
 
 
@@ -40,6 +35,7 @@ drvModbusAsynConfigure("readback_all", "MODBUS_IP", 1, 4, 20, 7, 0, 1000, "OCEM"
 
 # Load database records ## ports name are already define in db
 dbLoadRecords("$(TOP)/db/OCEMPowerSupply.db", "P=SPARC,R=OCEM:TEST,IMAX=500,VMAX=50")
+dbLoadRecords("$(TOP)/db/unimag-ocem.db", "P=SPARC,R=OCEM:TEST")
 
 iocInit()
 
